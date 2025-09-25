a95059692ba5a56fcdb5d29538cd52a87b27dc18
restsva-ui <restsva@gmail.com>
2025-09-25 23:08:00 +0300

    Update start.ts

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,4 +1,5 @@
 import { sendMessage } from "../telegram/api";
-export async function cmdStart(chatId: string|number) {
+
+export async function start(chatId: string | number) {
   await sendMessage(chatId, "👋 Привіт! Бот підключено до Cloudflare Workers.");
 }
