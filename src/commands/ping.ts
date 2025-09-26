f5b9ae8107bdd1a7f9bd02aa93c4590da1c1e023
restsva-ui <restsva@gmail.com>
2025-09-26 06:38:05 +0300

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,6 @@
+// src/commands/ping.ts
 import { sendMessage } from "../telegram/api";
 
-export async function ping(chatId: string | number) {
+export async function cmdPing(chatId: number | string) {
   await sendMessage(chatId, "pong ✅");
 }
