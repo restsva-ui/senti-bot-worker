cca6a9736167dd1e699e4f62d3f7c32d225a1a6b
restsva-ui <restsva@gmail.com>
2025-09-26 12:29:04 +0300

    Update menu.ts

--- src/commands/menu.ts
+++ src/commands/menu.ts
@@ -2,15 +2,11 @@
 import { sendMessage } from "../telegram/api";
 
 export async function menu(chatId: number) {
-  await sendMessage(
-    chatId,
-    "Головне меню:",
-    {
-      inline_keyboard: [
-        [{ text: "🔁 Ping", callback_data: "cb_ping" }],
-        [{ text: "👍 Лайки", callback_data: "cb_likepanel" }],
-        [{ text: "ℹ️ Допомога", callback_data: "cb_help" }],
-      ],
-    }
-  );
+  await sendMessage(chatId, "Головне меню:", {
+    inline_keyboard: [
+      [{ text: "🔁 Ping", callback_data: "cb_ping" }],
+      [{ text: "👍 Лайки", callback_data: "cb_likepanel" }],
+      [{ text: "ℹ️ Допомога", callback_data: "cb_help" }],
+    ],
+  });
 }
