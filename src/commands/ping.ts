fdde997b78b1cd60c739a86f2c2c60558c748995
restsva-ui <restsva@gmail.com>
2025-09-30 15:14:19 +0300

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,14 @@
 // src/commands/ping.ts
 import { tgSendMessage } from "../utils/telegram";
 
-export async function handlePing(env: any, chatId: number) {
-  // Відповідь на команду /ping
+/**
+ * Named export, якого чекає registry:
+ *   import pingNamed, { ping as pingExport } from "./ping";
+ * Ми даємо named "ping" і default (той самий) для сумісності.
+ */
+export async function ping(env: any, chatId: number) {
   await tgSendMessage(env, chatId, "pong ✅");
 }
+
+// Default export (ідентичний), щоб задовольнити import pingNamed, default
+export default ping;
