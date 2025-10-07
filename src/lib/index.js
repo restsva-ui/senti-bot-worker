2d0b92b7c37a29d0475f4d13a1b1a0644dc1be82
restsva-ui <restsva@gmail.com>
2025-10-07 17:00:46 +0300

    Create index.js

---
+++ src/lib/index.js
@@ -0,0 +1,8 @@
+// Єдина точка імпорту утиліт з /lib
+export * from "./state.js";
+export * from "./keyboard.js";
+
+// Якщо треба — додай інші реекспорти:
+// export * from "./drive.js";
+// export * from "./todo.js";
+// ...
