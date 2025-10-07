0bb63ee3d765ed87e0ec9a030355b68690c98899
restsva-ui <restsva@gmail.com>
2025-10-07 16:42:12 +0300

    Create keyboard.js

---
+++ src/lib/keyboard.js
@@ -0,0 +1,11 @@
+export function adminKeyboard() {
+  return {
+    keyboard: [
+      [{ text: "Drive ✅" }, { text: "List 10 📄" }],
+      [{ text: "Backup URL ⬆️" }, { text: "Checklist ➕" }],
+      [{ text: "Меню" }],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: false,
+  };
+}
