cede4810d8954d19495166747b0eea3840ae5cee
restsva-ui <restsva@gmail.com>
2025-10-07 17:05:48 +0300

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,3 @@
 // Єдина точка імпорту утиліт з /lib
 export * from "./state.js";
 export * from "./keyboard.js";
-
-// Якщо треба — додай інші реекспорти:
-// export * from "./drive.js";
-// export * from "./todo.js";
-// ...
