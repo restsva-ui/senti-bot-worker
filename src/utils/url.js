1b0ffd335b7963503d42d944ad64a1fe2505ab12
restsva-ui <restsva@gmail.com>
2025-10-20 12:50:34 +0300

    Delete src directory

--- src/utils/url.js
+++
@@ -1,8 +0,0 @@
-// src/utils/url.js
-
-/** Абсолютний URL до воркера за відносним шляхом */
-export function abs(env, path) {
-  if (!env?.SERVICE_HOST) throw new Error("SERVICE_HOST not set");
-  const p = path.startsWith("/") ? path : `/${path}`;
-  return `https://${env.SERVICE_HOST}${p}`;
-}
